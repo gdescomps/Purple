95f694460a5cb37799a14e3c8471266a33de868f
Jean deBonfils <jdbonfils@iutbayonne.univ-pau.fr>
2019-03-15 09:23:28 +0100

    Tirage d'un deck au demarrage

--- www/js/index.js
+++ www/js/index.js
@@ -16,11 +16,39 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 function deckAleatoire(){
     //alert("test");
     get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
 
 }
+
+function carteAleatoire(){
+    //alert("test");
+ 
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://deckofcardsapi.com/api/deck/"+deckId+"/draw/?count=2", true);
+    // .................................
+    request.onreadystatechange = function (aEvt) {
+        // .................................
+        if (request.readyState == 4) {
+            if (request.status == 200) {
+               var response = JSON.parse(request.responseText);
+                 var imagePrincipale=document.getElementById("imagePrincipale");
+                 var imageSecondaire=document.getElementById("imageSecondaire");
+                 imageSecondaire.setAttribute("src",imagePrincipale.src) ;
+                 var newsrc=response["cards"]["0"]["images"]["png"];
+                  imagePrincipale.setAttribute("src",newsrc);
+            }
+            else {  // .................................
+                console.log("Error : " + request.responseText);
+            }
+        }
+    };
+    request.send(null);
+
+}
+
 function get(url) {
     var request = new XMLHttpRequest();
     request.open("GET", url, true);
@@ -29,7 +57,7 @@ function get(url) {
         // .................................
         if (request.readyState == 4) {
             if (request.status == 200) {
-                onWebserviceSuccess(JSON.parse(request.responseText));
+               onWebserviceSuccess(JSON.parse(request.responseText));
             }
             else {  // .................................
                 console.log("Error : " + request.responseText);
@@ -37,13 +65,22 @@ function get(url) {
         }
     };
     request.send(null);
+   
 }
 
+
+var deckId;
 function onWebserviceSuccess(response) {
+  
     var newP = document.createElement("p");
-    newP.appendChild(document.createTextNode("Deck : " + response.deck_id));
+    deckId=response.deck_id;
+    newP.appendChild(document.createTextNode("Deck : " + deckId));
     document.getElementById("content").appendChild(newP);
+    
+    
 }
+
+
 
 var app = {
     // Application Constructor
@@ -58,7 +95,9 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         //this.receivedEvent('deviceready');
+        document.addEventListener("deviceready", deckAleatoire, false);
         document.getElementById("melanger").addEventListener("click", deckAleatoire );
+        document.getElementById("imagePrincipale").addEventListener("click", carteAleatoire );
     },
 
     // Update DOM on a Received Event
@@ -66,10 +105,8 @@ var app = {
         /*var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
         */
     }
