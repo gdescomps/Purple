83ae9cceb53a86943e6e23cd59561e83c4098d70
Jean deBonfils <jdbonfils@iutbayonne.univ-pau.fr>
2019-03-21 11:35:04 +0100

    gestion du backbutton

--- www/js/index.js
+++ www/js/index.js
@@ -180,7 +180,14 @@ function majScore(val){
     document.getElementById("pScore").innerHTML=score;
    
 }
-
+function onBackKeyDown()
+{
+    if (confirm("Voulez-vous quitter l'application ?")) 
+    {
+        // Code à éxécuter si le l'utilisateur clique sur "OK"
+        navigator.app.exitApp() ;
+    }
+}
 
 
 var app = {
@@ -203,6 +210,7 @@ onDeviceReady: function() {
         document.getElementById("rouge").addEventListener("click", function() {traiterChoixJoueurEtTirerUneCarte("rouge");}); 
         document.getElementById("noire").addEventListener("click", function() {traiterChoixJoueurEtTirerUneCarte("noire");});
         document.getElementById("purple").addEventListener("click", function() {traiterChoixJoueurEtTirerUneCarte("purple");});
+        document.addEventListener("backbutton", onBackKeyDown, false);
         document.getElementById("cartePrincipale").addEventListener("click", tirerCarte );
 },
     // Update DOM on a Received Event
